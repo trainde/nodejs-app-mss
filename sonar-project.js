ef4aebd5aeaf8a564dc2ff9687767e07943ac17e
Mithun Technologies <97269659+trainde@users.noreply.github.com>
2022-10-07 11:10:04 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.234.95.8:9000/',
+  serverUrl: 'http://34.224.57.210:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
